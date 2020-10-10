d0d0a969e97ab095bd9098915ddf90b46db1d349
abdelrahman maged <a_b_z@windowslive.com>
2020-10-10 15:46:05 -0400

    l

--- Student.cs
+++ Student.cs
@@ -37,15 +37,15 @@ namespace AM_A1
             string StuChoice = Console.ReadLine();
 
 
-            while (StuChoice != "0")
+            while (StuChoice != "5")
             {
 
                 switch (StuChoice)
                 {
                     case "1":
                         showAll<Student>(stLi);
-                            break;
-                   
+                        break;
+
                     case "2":
                         Console.WriteLine("Please enter in new details in the following order: Name,Residence and email.\n");
                         Console.WriteLine("Name:");
@@ -56,28 +56,50 @@ namespace AM_A1
                         string addCity = Console.ReadLine();
                         Console.WriteLine("Email:");
                         string addEmail = Console.ReadLine();
-                        Add<Student>(stLi,new Student(addName,new Address(addAddress1,addAddress2,addCity),addEmail));
+                        Add<Student>(stLi, new Student(addName, new Address(addAddress1, addAddress2, addCity), addEmail));
                         Console.WriteLine("New List\n");
                         showAll<Student>(stLi);
-                           
+
                         break;
 
 
                     case "3":
-                                       
-                Console.WriteLine("Enter the name and email of the student youd like to update\n")
-                Console.WriteLine("Name");
-                     string upName = Console.ReadLine();
-                Console.WriteLine("")
-                
+
+                        Console.WriteLine("Enter the name and email of the student youd like to update\n");
+
+                        Console.WriteLine("Name");
+                        string findName = Console.ReadLine();
+                        Console.WriteLine("Email:");
+                        
+
+
+                                             //   Update(upName)
+                        Console.WriteLine("New List");
+                        showAll<Student>(stLi);
+
+                        break;
+
+
+                    case "4":
+                        showAll<Student>(stLi);
+                        Console.WriteLine("Please enter the number that corrosponds to the students position on the list:(Ascending)\n");
+                        string delChoice=Console.ReadLine();
+                        Convert.ToInt32(delChoice);
+                        
+                        
+                        
+                        Console.WriteLine("New List");
+                        showAll<Student>(stLi);
+
+
+                        break;
                 }
 
 
 
 
 
- Console.WriteLine("Please enter a number that corsponds to Students position on the list:");
-
+                
 
 
 
@@ -89,51 +111,88 @@ namespace AM_A1
             }
         }
 
-    
 
-    public static void Update(String name, String email, List<Student> list)
-    {
 
-        var stu = from s in list
-                  where s.Name == name && s.Email == email
-                  select s;
-        Console.WriteLine("Please enter in new details in the following order: Name,Residence and email.\n");
-        Console.WriteLine("Name:");
-        string newName = Console.ReadLine();
-        Console.WriteLine("Address one,two and City");
-        string Address1 = Console.ReadLine();
-        string Address2 = Console.ReadLine();
-        string City = Console.ReadLine();
-        Console.WriteLine("Email:");
-        string newEmail = Console.ReadLine();
-        stu.ToList();
-        foreach (Student st in stu) {
+        public static void Update(String name, String email, List<Student> list)
+        {
 
-            st.Email = newEmail;
-            st.Residence.Address1 = Address1;
-            st.Residence.Address2 = Address2;
-            st.Residence.City = City;
-            st.Name = newName;
+            var stu = from s in list
+                      where s.Name == name && s.Email == email
+                      select s;
+            Console.WriteLine("Please enter in new details in the following order: Name,Residence and email.\n");
+            Console.WriteLine("Name:");
+            string newName = Console.ReadLine();
+            Console.WriteLine("Address one,two and City");
+            string Address1 = Console.ReadLine();
+            string Address2 = Console.ReadLine();
+            string City = Console.ReadLine();
+            Console.WriteLine("Email:");
+            string newEmail = Console.ReadLine();
+            stu.ToList();
+            foreach (Student st in stu)
+            {
+
+                st.Email = newEmail;
+                st.Residence.Address1 = Address1;
+                st.Residence.Address2 = Address2;
+                st.Residence.City = City;
+                st.Name = newName;
+
+            }
+            // Student student = new Student(name, new Address(Address1, Address2, City), email);
+            //list.Add(student);
+
+
+            Console.WriteLine("New Student information:");
+
+
+
+
+
+
+
+
+
+
+
 
         }
-        // Student student = new Student(name, new Address(Address1, Address2, City), email);
-        //list.Add(student);
-
-
-        Console.WriteLine("New Student information:");
-
-
-
-
-
-
-
-
-
-
-
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
@@ -151,42 +210,6 @@ namespace AM_A1
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 
 
 
